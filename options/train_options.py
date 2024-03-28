c61117a9b9acab50701bb81bb093540d812c0e55
Mohamed Walid <Moh.Walid2002@gmail.com>
2024-03-28 14:50:48 +0200

    Update train_options.py

--- options/train_options.py
+++ options/train_options.py
@@ -41,7 +41,7 @@ class TrainOptions(BaseOptions):
         self.parser.add_argument(
             '--beta1', type=float, default=0.5, help='momentum term of adam')
         self.parser.add_argument(
-            '--lr', type=float, default=0.00005, help='initial learning rate for adam')
+            '--lr', type=float, default=0.000047, help='initial learning rate for adam')
         self.parser.add_argument(
             '--lr_D', type=float, default=0.00005, help='initial learning rate for adam')
         self.parser.add_argument('--pretrain_checkpoint_D', type=str, default='/kaggle/input/3-epochs/flow/PBAFN_D_epoch_079.pth',
