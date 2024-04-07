97e90a3333242f3de812eee3d3e46f7f0fae673a
Mohamed Ahmed <88492114+SEmohamedAhmed@users.noreply.github.com>
2024-04-07 03:42:47 +0200

    115

--- options/train_options.py
+++ options/train_options.py
@@ -41,16 +41,16 @@ class TrainOptions(BaseOptions):
         self.parser.add_argument(
             '--beta1', type=float, default=0.5, help='momentum term of adam')
         self.parser.add_argument(
-            '--lr', type=float, default=1.99713222426176e-05, help='initial learning rate for adam')
+            '--lr', type=float, default=1.9172469352912894e-05, help='initial learning rate for adam')
         self.parser.add_argument(
-            '--lr_D', type=float, default=0.000022, help='initial learning rate for adam')
-        self.parser.add_argument('--pretrain_checkpoint_D', type=str, default='/kaggle/input/3-epochs/flow/PBAFN_D_epoch_112.pth',
+            '--lr_D', type=float, default=0.000021, help='initial learning rate for adam')
+        self.parser.add_argument('--pretrain_checkpoint_D', type=str, default='/kaggle/input/3-epochs/flow/PBAFN_D_epoch_115.pth',
                                  help='load the pretrained model from the specified location')
         self.parser.add_argument('--PFAFN_warp_checkpoint', type=str,
                                  help='load the pretrained model from the specified location')
         self.parser.add_argument('--PFAFN_gen_checkpoint', type=str,
                                  help='load the pretrained model from the specified location')
-        self.parser.add_argument('--PBAFN_warp_checkpoint', type=str, default= '/kaggle/input/3-epochs/flow/PBAFN_warp_epoch_112.pth', 
+        self.parser.add_argument('--PBAFN_warp_checkpoint', type=str, default= '/kaggle/input/3-epochs/flow/PBAFN_warp_epoch_115.pth', 
                                  help='load the pretrained model from the specified location')
         self.parser.add_argument('--PBAFN_gen_checkpoint', type=str,
                                  help='load the pretrained model from the specified location')
