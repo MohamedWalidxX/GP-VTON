fc98c96427afe0f799b5f0d224c16dfa3b870ef8
Mohamed Ahmed <88492114+SEmohamedAhmed@users.noreply.github.com>
2024-04-07 14:17:52 +0200

    118

--- options/train_options.py
+++ options/train_options.py
@@ -41,16 +41,16 @@ class TrainOptions(BaseOptions):
         self.parser.add_argument(
             '--beta1', type=float, default=0.5, help='momentum term of adam')
         self.parser.add_argument(
-            '--lr', type=float, default=1.9172469352912894e-05, help='initial learning rate for adam')
+            '--lr', type=float, default=1.8405570578796376e-05, help='initial learning rate for adam')
         self.parser.add_argument(
-            '--lr_D', type=float, default=0.000021, help='initial learning rate for adam')
-        self.parser.add_argument('--pretrain_checkpoint_D', type=str, default='/kaggle/input/3-epochs/flow/PBAFN_D_epoch_115.pth',
+            '--lr_D', type=float, default=0.000020, help='initial learning rate for adam')
+        self.parser.add_argument('--pretrain_checkpoint_D', type=str, default='/kaggle/input/3-epochs/flow/PBAFN_D_epoch_118.pth',
                                  help='load the pretrained model from the specified location')
         self.parser.add_argument('--PFAFN_warp_checkpoint', type=str,
                                  help='load the pretrained model from the specified location')
         self.parser.add_argument('--PFAFN_gen_checkpoint', type=str,
                                  help='load the pretrained model from the specified location')
-        self.parser.add_argument('--PBAFN_warp_checkpoint', type=str, default= '/kaggle/input/3-epochs/flow/PBAFN_warp_epoch_115.pth', 
+        self.parser.add_argument('--PBAFN_warp_checkpoint', type=str, default= '/kaggle/input/3-epochs/flow/PBAFN_warp_epoch_118.pth', 
                                  help='load the pretrained model from the specified location')
         self.parser.add_argument('--PBAFN_gen_checkpoint', type=str,
                                  help='load the pretrained model from the specified location')
