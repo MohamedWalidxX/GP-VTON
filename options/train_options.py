3f0df700454b0f6f9fbf03bae5314d02d8353a48
Mohamed Walid <Moh.Walid2002@gmail.com>
2024-04-25 10:21:44 +0200

    Update train_options.py

--- options/train_options.py
+++ options/train_options.py
@@ -41,16 +41,16 @@ class TrainOptions(BaseOptions):
         self.parser.add_argument(
             '--beta1', type=float, default=0.5, help='momentum term of adam')
         self.parser.add_argument(
-            '--lr', type=float, default=1.4823733267868212e-05, help='initial learning rate for adam')
+            '--lr', type=float, default=1.4267843270323152e-05, help='initial learning rate for adam')
         self.parser.add_argument(
-            '--lr_D', type=float, default=0.000014, help='initial learning rate for adam')
-        self.parser.add_argument('--pretrain_checkpoint_D', type=str, default='/kaggle/input/3-epochs/flow/PBAFN_D_epoch_139.pth',
+            '--lr_D', type=float, default=0.000013, help='initial learning rate for adam')
+        self.parser.add_argument('--pretrain_checkpoint_D', type=str, default='/kaggle/input/3-epochs/flow/PBAFN_D_epoch_142.pth',
                                  help='load the pretrained model from the specified location')
         self.parser.add_argument('--PFAFN_warp_checkpoint', type=str,
                                  help='load the pretrained model from the specified location')
         self.parser.add_argument('--PFAFN_gen_checkpoint', type=str,
                                  help='load the pretrained model from the specified location')
-        self.parser.add_argument('--PBAFN_warp_checkpoint', type=str, default= '/kaggle/input/3-epochs/flow/PBAFN_warp_epoch_139.pth', 
+        self.parser.add_argument('--PBAFN_warp_checkpoint', type=str, default= '/kaggle/input/3-epochs/flow/PBAFN_warp_epoch_142.pth', 
                                  help='load the pretrained model from the specified location')
         self.parser.add_argument('--PBAFN_gen_checkpoint', type=str,
                                  help='load the pretrained model from the specified location')
